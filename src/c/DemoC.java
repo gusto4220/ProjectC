6a8d77050517339df7a12ad87dc91a19525e6966
gustavocruz <gustavocruz@gustavos-air.attlocal.net>
2024-01-03 19:56:53 -0600

    First Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,11 @@
+package c;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gustavo");
+		System.out.println("Day1");
+		System.out.println("Day2");
+	}
+}
